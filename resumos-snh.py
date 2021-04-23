b4ea0fde7ed49924e8326a830c46506e38ad92c9
ericbrasiln <ericbrasiln@protonmail.com>
2021-04-23 17:00:04 -0300

    atualização do código

--- resumos-snh.py
+++ resumos-snh.py
@@ -1,18 +1,27 @@
-from urllib.request import Request, urlopen, urlretrieve
+"""
+Código para raspagem dos resumos submetidos aos Simpósio nacionais de História da Anpuh entre 2013 e 2019.
+Autoria: Eric Brasil (IHL-UNILAB, LABHDUFBA)
+"""
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-import re
-import os
-import urllib
 import pandas as pd
+from datetime import datetime
 from pprint import pprint
 
 dic = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
+now = datetime.now()
+date = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+print('\nCódigo para raspagem dos resumos submetidos aos Simpósio nacionais de História da Anpuh entre 2013 e 2019.\n'
+      'Autoria: Eric Brasil\n')
+print('-=-'*30)
 
 snhYears= list()
 while True:
-    snhYears.append(int(input('Digite o ano do evento [entre 2013, 2015, 2017 e 2019]: ')))
-    resp = str(input('Deseja inserir outro ano? [S/N] '))
+    snhYears.append(int(input('Digite o ano do primeiro evento [entre 2013, 2015, 2017 e 2019]que deseja raspar: ')))
+    resp = str(input('Deseja inserir outro ano para raspagem? [S/N] '))
     if resp in 'Nn':
+        print('-=-'*50)
         break
 snhYears.sort()
 
@@ -22,11 +31,23 @@ for r in ",[]":
 snhList = snhList.replace(' ','_')
 
 listaFinal=[]
-def saveDF(listaFinal):
-    df = pd.DataFrame(listaFinal, columns=['Ano', 'ST', 'Coordenadores', 'Autor(es)/Instituições', 'Título', 'Resumo'])
-    df.to_csv(f'anais-anpuh-resumos-{snhList}.csv')
+def saveDF(listaFinal,date):
+    df = pd.DataFrame(listaFinal, columns=['Ano', 'Evento', 'Cidade', 'ST', 'Coordenadores', 'Autor(es)/Instituições', 'Título', 'Resumo'])
+    df.to_csv(f'resumos_anpuh_{snhList}_{date}.csv')
 
 def getAbstract(bs, year, listaFinal):
+    if year == 2013:
+        city = 'Natal'
+        event = 'XXVII'
+    if year == 2015:
+        city = 'Florianópolis'
+        event = 'XXVIII'
+    if year == 2017:
+        city = "Brasília"
+        event = 'XXIX'
+    if year == 2019:
+        city = 'Recife'
+        event = 'XXX'
     if year in [2013, 2015]:
         STContent = bs.find(class_='content-interna')
     else:
@@ -42,11 +63,9 @@ def getAbstract(bs, year, listaFinal):
         title = paper.find('b').text
         if year in [2013, 2015]:
             abstract = paper.find(style='font-size:11px;').text.strip()
-            print(f'Autor(es): {author}\nTítulo: {title}\nResumo: {abstract}\n')
         else:
             abstract = paper.find(style='display:none;font-size:11px;').text.strip()
-            print(f'Autor(es): {author}\nTítulo: {title}\nResumo: {abstract}\n')
-        listaInterna = [year, STtitle, coordinators, author, title, abstract]
+        listaInterna = [year, event, city, STtitle, coordinators, author, title, abstract]
         listaFinal.append(listaInterna)
 
 def stList(bs, className, year):
@@ -55,7 +74,6 @@ def stList(bs, className, year):
         try:
             ST = ST.find('h3')
             STlink = ST.find('a')['href']
-            STtitle = ST.text
             pprint(STlink)
             reqopen = Request(STlink)
             req = urlopen(reqopen)
@@ -73,14 +91,7 @@ def request (url, dic, year):
 def baseUrl(snhYears):
     for year in snhYears:
         url = f'http://snh{year}.anpuh.org/simposio/public'
-        #pasta = os.path.join('Anais  Anpuh', 'resumos')
-        #if not os.path.exists(pasta):
-        #    os.makedirs(pasta)
-        #pastaEvento = os.path.join(pasta, f'SNH_{year}')
-        #if not os.path.exists(pastaEvento):
-        #    os.makedirs(pastaEvento)
         request(url,dic, year)
 
 baseUrl(snhYears)
-saveDF(listaFinal)
-
+saveDF(listaFinal,date)
